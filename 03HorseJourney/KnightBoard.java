734eff9cef49119d4f5d103e9202485985313424
WhatABbas <aabbas@stuy.edu>
2017-02-17 22:59:42 -0500

    It doesn't work on 4x4 boards I was right all along...

--- 03HorseJourney/KnightBoard.java
+++ 03HorseJourney/KnightBoard.java
@@ -1,73 +1,61 @@
 public class KnightBoard{
     int[][] board;
-    
+
     public KnightBoard(int startingRows, int startingCols){
-	board = new int[startingRows][startingCols];
+	       board = new int[startingRows][startingCols];
     }
 
     public void solve(){
-	solveH((int)Math.random() * board.length, (int)Math.random() * board[0].length, 1);
+	       solveH(0, 0, 1);
     }
 
-    public boolean solveH(int row, int col, int level){	
-	System.out.println(this);
-	
-	if(level - 1 == board.length * board[0].length){
-	    return true;
-	}
-	try{
-	    if(board[row][col] != 0){
-		return false;
-	    }
-	}
-	catch(IndexOutOfBoundsException e){
-	    return false;
-	}
+    public boolean solveH(int row, int col, int level){
 
-	board[row][col] = level;
-	
-	level = level + 1;
+    	if(level == board.length * board[0].length + 1){
+            System.out.println(this);
+    	    return true;
+    	}
 
-	      if(!solveH(row + 2, col + 1, level)){
-		if(!solveH(row + 2, col - 1, level)){
-		    if(!solveH(row - 2, col + 1, level)){
-			if(!solveH(row - 2, col - 1, level)){
-			    if(!solveH(row + 1, col + 2, level)){
-				if(!solveH(row + 1, col - 2, level)){
-				    if(!solveH(row - 1, col + 2, level)){
-					if(!solveH(row - 1, col - 2, level)){
-					    board[row][col] = 0;
-					    return false;
-					}
-				    }
-				}
-			    }
-			}
-		    }
-		}
-	      }
-	      return true;
+    	try{
+    	    if(board[row][col] != 0){
+                return false;
+    	    }
+    	}
+    	catch(IndexOutOfBoundsException e){
+    	    return false;
+    	}
+
+    	board[row][col] = level;
+
+    	level = level + 1;
+
+        if(!(solveH(row + 2, col + 1, level) || solveH(row + 2, col - 1, level) || solveH(row - 2, col + 1, level) || solveH(row - 2, col - 1, level) || solveH(row + 1, col + 2, level) || solveH(row + 1, col - 2, level) || solveH(row - 1, col + 2, level) || solveH(row - 1, col - 2, level))){
+                					    board[row][col] = 0;
+                					    return false;
+        }
+
+        return true;
     }
 
     public String toString(){
-	String toRet = "";
-	for(int r = 0; r < board.length; r++){
-	    for(int c = 0; c < board.length; c++){
-		if(board[r][c] < 10){
-		    toRet += " " + board[r][c];
-		}
-		else{
-		    toRet += "" + board[r][c];
-		}
-		toRet += " ";
-	    }
-	    toRet += "\n";
-	}
-	return toRet;
+    	String toRet = "";
+    	for(int r = 0; r < board.length; r++){
+    	    for(int c = 0; c < board.length; c++){
+        		if(board[r][c] < 10){
+        		    toRet += " " + board[r][c];
+        		}
+        		else{
+        		    toRet += "" + board[r][c];
+        		}
+        		toRet += " ";
+    	    }
+    	    toRet += "\n";
+    	}
+    	return toRet;
     }
 
     public static void main(String[] args){
-	KnightBoard test = new KnightBoard(4, 4);
+	KnightBoard test = new KnightBoard(6,6);
 
 	test.solve();
 
@@ -75,5 +63,3 @@ public class KnightBoard{
     }
 
 }
-
-

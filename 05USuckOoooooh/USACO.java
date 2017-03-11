3877d1b3327b20d4590522c03d3d67af3def6a2b
Watabaws <aabbas@stuy.edu>
2017-03-11 12:06:36 -0500

    Silver works!

--- 05USuckOoooooh/USACO.java
+++ 05USuckOoooooh/USACO.java
@@ -3,8 +3,9 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class USACO{
-    private int row, col, elev, numStomps;
+    private int row, col, elev, numStomps, time, R1, R2, C1, C2;
     private int[][] pasture, moves;
+    private int[][][] pastur;
 
     public USACO(){}
 
@@ -90,6 +91,94 @@ public class USACO{
         return total;
     }
 
+    public int silver(String filename){
+        Scanner inf = null;
+        try{
+            inf = new Scanner(new File(filename));
+        }
+        catch(FileNotFoundException e){
+            System.exit(0);
+        }
+        row = inf.nextInt();
+        col = inf.nextInt();
+        time = inf.nextInt();
+        inf.nextLine();
+        pastur = new int[row][col][2];
+        for(int i = 0; i < row; i++){
+            int[] valus = converCI(inf.nextLine().toCharArray());
+            for(int j = 0; j < valus.length; j++){
+                pastur[i][j] = new int[]{valus[j], valus[j]};
+            }
+        }
+        R1 = inf.nextInt() - 1;
+        C1 = inf.nextInt() - 1;
+        R2 = inf.nextInt() - 1;
+        C2 = inf.nextInt() - 1;
+        //printThreeDAr(pastur);
+        return calcPaths(R1, C1, R2, C2);
+    }
+
+    private int calcPaths(int startRow, int startCol, int endRow, int endCol){
+        pastur[startRow][startCol] = new int[]{1,1};
+        while(time > 0){
+            for(int r = 0; r < pastur.length; r++){
+                for(int c = 0; c < pastur[r].length; c++){
+                    if(pastur[r][c][0] != -1){
+                        pastur[r][c][1] = 0;
+                        try{
+                            if(pastur[r+1][c][0] != -1){
+                                pastur[r][c][1] += pastur[r+1][c][0];
+                            }
+                        }
+                        catch(IndexOutOfBoundsException e){}
+                        try{
+                            if(pastur[r-1][c][0] != -1){
+                                pastur[r][c][1] += pastur[r-1][c][0];
+                            }
+                        }
+                        catch(IndexOutOfBoundsException e){}
+                        try{
+                            if(pastur[r][c+1][0] != -1){
+                                pastur[r][c][1] += pastur[r][c+1][0];
+                            }
+                        }
+                        catch(IndexOutOfBoundsException e){}
+                        try{
+                            if(pastur[r][c-1][0] != -1){
+                                pastur[r][c][1] += pastur[r][c-1][0];
+                            }
+                        }
+                        catch(IndexOutOfBoundsException e){}
+                    }
+                }
+            }
+            for(int r = 0; r < pastur.length; r++){
+                for(int c = 0; c < pastur[r].length; c++){
+                    pastur[r][c][0] = pastur[r][c][1];
+                }
+            }
+            //printThreeDAr(pastur);
+            //System.out.println("");
+            time--;
+        }
+
+        return pastur[endRow][endCol][0];
+    }
+
+
+    private static int[] converCI(char[] toConv){
+        int[] toRet = new int[toConv.length];
+        for(int i = 0; i < toConv.length; i++){
+            if(toConv[i] == '.'){
+                toRet[i] = 0;
+            }
+            if(toConv[i] == '*'){
+                toRet[i] = -1;
+            }
+        }
+        return toRet;
+    }
+
     private void printTwoDAr(int[][] toPrint){
         String toRet = "";
         for(int r = 0; r < toPrint.length;r++){
@@ -101,9 +190,25 @@ public class USACO{
         System.out.println(toRet);
     }
 
+    private void printThreeDAr(int[][][] toPrint){
+        String toRet = "";
+        for(int r = 0; r < toPrint.length; r++){
+            for(int c = 0; c < toPrint[r].length; c++){
+                for(int z = 0; z < toPrint[r][c].length; z++){
+                    toRet += toPrint[r][c][z];
+                    toRet += " ";
+                }
+                toRet += "   ";
+            }
+            toRet += "\n";
+        }
+        System.out.println(toRet);
+    }
+
     public static void main(String[] args){
         USACO x = new USACO();
 
-        System.out.println(x.bronze(args[0]));
+        System.out.println(x.silver(args[0]));
+        //System.out.println(x.bronze(args[0]));
     }
 }

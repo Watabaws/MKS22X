83cb344c4e28287f82e241f5675d15a911ec0f62
Watabaws <aabbas@stuy.edu>
2017-03-28 14:16:55 -0400

    LinkedList work and better 08 name

--- 09LonkedLost/MyLinkedList.java
+++ 09LonkedLost/MyLinkedList.java
@@ -2,16 +2,69 @@ public class MyLinkedList{
     private LNode start;
     private int size;
 
+
     public MyLinkedList(){
         start = new LNode();
         size = 1;
     }
 
-    private void add(LNode tba){
+    public int size(){
+        return size;
+    }
+
+    private boolean addBeginning(LNode tba){
         tba.next = start;
         start = tba;
         size++;
+        return true;
     }
+
+    public boolean add(int tba){
+        current = start;
+        while(current.next != void){
+            current = current.next;
+        }
+        current.next = new LNode(tba);
+    }
+
+    public int get(int index){
+        current = start;
+        while(index == 0){
+            current = current.next;
+            index--;
+        }
+        return current.value;
+    }
+
+    public int set(int index, int newValue){
+        current = start;
+        while(index == 0){
+            current = current.next;
+            index--;
+        }
+        int oldVal = current.value;
+        current.value = newValue;
+        return oldVal;
+    }
+
+    public int indexOf(int val){
+        current = start;
+        ctr = 0;
+        while(current.value != val){
+            current = current.next;
+            ctr++;
+        }
+        return ctr;
+    }
+
+    public boolean add(int index, int value){
+        current = start;
+        for(int i = 0; i < index - 1; i++){
+            current = current.next;
+        }
+
+    }
+
 
     public String toString(){
         LNode current = start;

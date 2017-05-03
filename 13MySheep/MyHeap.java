6ae06616942ef64b0b8c8c14c340d99bc5a5d584
Adam Abbas <aabbas@stuy.edu>
2017-05-03 14:47:24 -0400

    Set up the skeleton of the MyHeap

---
+++ 13MySheep/MyHeap.java
@@ -0,0 +1,32 @@
+public class MyHeap{
+    private int Constant = 1;
+    private int size;
+    private ArrayList<String> sheep;
+
+    public MyHeap(){
+	size = 0;
+	sheep = new ArrayList<String>;
+    }
+
+    public MyHeap(boolean){
+	Constant = -1;
+	size = 0;
+	sheep = new ArrayList<String>;
+    }
+
+    public void add(String s){
+	
+    }
+
+    public String remove(){
+
+    }
+
+    public String peek(){
+
+    }
+}
+
+
+
+    

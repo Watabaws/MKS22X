de7fa2c1c0ece446ab08c975b6301523e8694ad6
Watabaws <aabbas@stuy.edu>
2017-05-10 21:36:26 -0400

    I don't even know what's working anymore

--- 14WalkingMaximum/MyHeap.java
+++ 14WalkingMaximum/MyHeap.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class MyHeap{
     private int Constant = 1;
     public int size;
-    private ArrayList<Integer> sheep;
+    public ArrayList<Integer> sheep;
 
     public MyHeap(){
         size = 0;
         sheep = new ArrayList<Integer>();
-        sheep.add(0);
+        sheep.add(null);
     }
 
     public MyHeap(boolean huh){
@@ -21,24 +21,23 @@ public class MyHeap{
     public void add(Integer s){
         sheep.add(s);
         size++;
-        if(size > 1){
-            doAPullUP(size);
-        }
+        doAPullUP(size);
     }
 
     public Integer remove(){
         Integer oldval = sheep.get(1);
-        sheep.set(1, sheep.get(size--));
+        sheep.set(1, sheep.remove(size));
+        size -= 1;
         doAPushUpButStayDownSoItsNotReallyAPushup(1);
         return oldval;
     }
 
     public Integer peek(){
-        return sheep.get(0);
+        return sheep.get(1);
     }
 
     private boolean actuallySortaUsefulCompareTo(Integer a, Integer b){
-        return a.compareTo(b) * Constant >= 0;
+        return (a.compareTo(b) * Constant) >= 0;
     }
 
     private void doAPullUP(int index){
@@ -51,63 +50,57 @@ public class MyHeap{
     }
 
     private void doAPushUpButStayDownSoItsNotReallyAPushup(int index){
-        Integer mwa = sheep.get(index);
-        System.out.println("Size" + size);
-        if(index * 2 <= size){
+        if(index * 2 + 1 <= size){
+            Integer mwa = sheep.get(index);
             Integer chil1 = sheep.get(index * 2);
-            if(index * 2 + 1 <= size){
-                Integer chil2 = sheep.get(index * 2 + 1);
-                if(actuallySortaUsefulCompareTo(chil1, chil2)){
-                    shwitch(index, index * 2);
-                    doAPushUpButStayDownSoItsNotReallyAPushup(index * 2);
-                }
-                else{
-                    shwitch(index, index * 2 + 1);
-                    doAPushUpButStayDownSoItsNotReallyAPushup(index * 2 + 1);
-                }
+            Integer chil2 = sheep.get(index * 2 + 1);
+            int maxVal;
+
+            if(actuallySortaUsefulCompareTo(chil1, chil2)){
+                maxVal = index * 2;
+                chil2 = chil1;
             }
+            else{
+                maxVal = index * 2 + 1;
+                chil1 = chil2;
+            }
+
             if(actuallySortaUsefulCompareTo(chil1, mwa)){
-                shwitch(index * 2, index);
+                sheep.set(maxVal, mwa);
+                sheep.set(index, chil1);
+                doAPushUpButStayDownSoItsNotReallyAPushup(maxVal);
             }
+
         }
     }
 
-    private void shwitch(int in, int ol){
-        Integer old = sheep.get(ol);
-        sheep.set(in, sheep.get(ol));
-        sheep.set(ol, old);
-    }
-
-    public String toString(){
+    public String toStringAttemptFailure(){
         String toret = "";
         int looptru = (int)Math.ceil((Math.log(size) / Math.log(2)));
         int og = looptru;
-        int row = 0;
+        int row = 1;
         while(looptru != 0){
-            row++;
             String tempret = "";
             for(int i = looptru; i <= og; i++){
                 tempret += "   ";
             }
-            for(int i = (int) Math.pow(2, looptru - 1); i < Math.pow(2, looptru); i++){
-                tempret += sheep.get(i) + "    ";
+            for(int i = (int) Math.pow(2, row-1); i < Math.pow(2, row); i++){
+                tempret +=  "    " + sheep.get(i);
             }
             tempret += "\n";
-            toret = tempret + toret;
+            toret += tempret ;
             looptru--;
+            row++;
         }
         return toret;
     }
 
-    public String loserToString(){
-        String toret = "  ";
-        for(int i = 0; i < size; i++){
-            Integer topr = sheep.get(i+1);
-            if(topr != null){
-                toret += topr + " ";
-            }
+    public String toString(){
+        String toret = "";
+        for(int i = 1; i <= size; i++){
+            Integer topr = sheep.get(i);
+            toret += topr + " ";
         }
         return toret;
     }
-
 }
--- 14WalkingMaximum/RunningMedian.java
+++ 14WalkingMaximum/RunningMedian.java
@@ -5,14 +5,27 @@ public class RunningMedian{
     public RunningMedian(){
         maks = new MyHeap(true);
         meen = new MyHeap(false);
+        maxSize = 0;
     }
 
     public void add(int chewAd){
-        if(chewAd < meen.peek()){
-            maks.add(chewAd);
+        if(maks.size != 0 && meen.size != 0){
+            System.out.println(maks.sheep);
+            System.out.println(meen.sheep);
+            if(chewAd < meen.peek()){
+                maks.add(chewAd);
+            }
+            else{
+                meen.add(chewAd);
+            }
         }
         else{
-            meen.add(chewAd);
+            if(maks.size == 0){
+                maks.add(chewAd);
+            }
+            else{
+                meen.add(chewAd);
+            }
         }
         balance();
         updateSize();
@@ -45,5 +58,17 @@ public class RunningMedian{
 
     private void updateSize(){
         maxSize = maks.size + meen.size;
+    }
+
+    public static void main(String[] args){
+        RunningMedian test = new RunningMedian();
+        int[] testnum = {0, 2, 5, 6, 7, 234, 342};
+        for(int i : testnum){
+        System.out.println(test.maks);
+        System.out.println(test.meen);
+            test.add(i);
+            System.out.println("");
+        }
+        System.out.println(test.getMedian());
     }
 }
